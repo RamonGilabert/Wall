78d32d287f574fb701c47772c0134b3ffda0eb6b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-10-26 09:38:59 +0100

    Adding some more files

---
+++ Source/Views/PostInformationBarView.swift
@@ -0,0 +1,21 @@
+//
+//  WallInformationBarView.swift
+//  Pods
+//
+//  Created by Ramon Gilabert Llop on 10/26/15.
+//
+//
+
+import UIKit
+
+class WallInformationBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
