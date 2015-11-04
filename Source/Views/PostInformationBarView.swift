c0620953598e8565e3717547a73a97b97e74b7f7
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-04 10:32:24 +0100

    Adds some configuration

--- Source/Views/PostInformationBarView.swift
+++ Source/Views/PostInformationBarView.swift
@@ -2,6 +2,12 @@ import UIKit
 
 public class PostInformationBarView: UIView {
 
+  public struct Dimensions {
+    public static let offset: CGFloat = 24
+    public static let topOffset: CGFloat = 18
+    public static let interitemOffset: CGFloat = 10
+  }
+
   public lazy var likesLabel: UILabel = {
     let label = UILabel()
     label.font = UIFont.systemFontOfSize(12)
@@ -39,9 +45,9 @@ public class PostInformationBarView: UIView {
   }
 
   public override func drawRect(rect: CGRect) {
-    [likesLabel, commentsLabel, seenLabel].forEach { $0.sizeToFit() }
-    
     super.drawRect(rect)
+
+    [likesLabel, commentsLabel, seenLabel].forEach { $0.sizeToFit() }
 
     likesLabel.frame.origin = CGPoint(x: 24, y: 18)
     commentsLabel.frame.origin = CGPoint(x: CGRectGetMaxX(likesLabel.frame) + 10, y: 18)
