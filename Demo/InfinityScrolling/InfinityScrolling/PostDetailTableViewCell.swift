bf81682810bec2198dbd24ff26ce68ccb4bd203a
Vadym Markov <markov.vadym@gmail.com>
2015-11-05 17:19:16 +0100

    Put comment controller

---
+++ Demo/InfinityScrolling/InfinityScrolling/PostDetailTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PostDetailTableViewCell.swift
+//  InfinityScrolling
+//
+//  Created by Vadym Markov on 05/11/15.
+//  Copyright © 2015 Ramon Gilabert Llop. All rights reserved.
+//
+
+import Foundation
