3955fb4fc1db789e5280549979c85c5b02e88f61
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-06 16:23:44 +0100

    Adds the main configuration for the posts and stuff

--- Source/Library/FontList.swift
+++ Source/Library/FontList.swift
@@ -1,1 +1,27 @@
 import UIKit
+
+public struct FontList {
+
+  public struct Post {
+    public static var author = UIFont.boldSystemFontOfSize(14)
+    public static var date = UIFont.systemFontOfSize(12)
+    public static var text = UIFont.systemFontOfSize(14)
+  }
+
+  public struct Comment {
+    public static var author = UIFont.boldSystemFontOfSize(14)
+    public static var text = UIFont.systemFontOfSize(14)
+    public static var date = UIFont.systemFontOfSize(12)
+  }
+
+  public struct Information {
+    public static var like = UIFont.systemFontOfSize(12)
+    public static var comment = UIFont.systemFontOfSize(12)
+    public static var seen = UIFont.systemFontOfSize(12)
+  }
+
+  public struct Actions {
+    public static var like = UIFont.boldSystemFontOfSize(14)
+    public static var comment = UIFont.boldSystemFontOfSize(14)
+  }
+}
